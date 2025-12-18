f0aabe76612f9fe1c38d43773e3cba3ace5dacfd
Seseda13 <arjikeys@gmail.com>
2025-12-18 12:44:36 +0500

    Реализация капчи

---
+++ ajax/regin_user.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require_once("./settings/connect_database.php");
+require_once("./libs/autoload.php");
+
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+// ищем пользователя
+$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login` = '".$login."'");
+$id = $query_user->fetch_row();
+
+if($user_read = $query_user->fetch_row()) {
+    echo $id;
+} else {
+    if(isset($_POST["g-recaptcha-response"]) == false) {
+        echo "Нет проверки 'Я не робот'";
+    } exit;
+
+    $secret = "6LfcXC8sAAAAABeo2fnjnUCDHh4-0PyAl5SIr2Is";
+    $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+
+    $response = $recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
+
+    if($response->isSuccess()) {
+        $mysqli->query("INSERT INTO `users` (`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', @)");
+        
+        $query_user = $mysqli->query("SELECT * FROM `users` WHERE `login` = '".$login."' AND `password` = '".$password."'");
+        $user_new = $query_user->fetch_row();
+        $id = $user_new[0];
+
+        if($id != -1) $_SESSION['user'] = $id; // запоминаем пользователя
+        echo $id;
+    } else {
+        echo "Пользователь не распознан.";
+    } exit;
+}
+?>
